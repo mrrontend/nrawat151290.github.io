2cb4d7c7b498f17ca65370b273c1d1a3bf4ea6cd
nirawat <nipun.rawat@gartner.com>
2020-04-07 22:44:36 +0530

    excluded core js from babel loader

---
+++ precache-manifest.794e97afb59d008e01a99563bb3608f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d935fc06da739de49fdfd2d85d2e99a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401fd0970a5ab2b4b6a9",
+    "url": "/static/css/main.3d0097d9.chunk.css"
+  },
+  {
+    "revision": "3cf035613cfb5b93ab8b",
+    "url": "/static/js/2.841b1117.chunk.js"
+  },
+  {
+    "revision": "73a4d48476e18a131991eddddf739f24",
+    "url": "/static/js/2.841b1117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401fd0970a5ab2b4b6a9",
+    "url": "/static/js/main.c8ef6e0b.chunk.js"
+  },
+  {
+    "revision": "2d8f72aeca456b536118",
+    "url": "/static/js/runtime-main.c2717fbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ff299a96d2056108484345182b67a27.js"
+  "/precache-manifest.794e97afb59d008e01a99563bb3608f6.js"
 );
 
 self.addEventListener('message', (event) => {
