bd54361640e59fc69ac1083d34467ff2a5910615
nirawat <nipun.rawat@gartner.com>
2020-04-08 08:49:23 +0530

    added support for 320px screens

---
+++ precache-manifest.3f9abe14b979965315dbd6a4908f03c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc8496973e3b6ed0d310ddc240d6fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7246d9c82f91e7449db1",
+    "url": "/static/css/main.2072db94.chunk.css"
+  },
+  {
+    "revision": "926a985c16925f05f786",
+    "url": "/static/js/2.6d5b0414.chunk.js"
+  },
+  {
+    "revision": "8a35e35e00e763aabcd15e61b0cb2343",
+    "url": "/static/js/2.6d5b0414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7246d9c82f91e7449db1",
+    "url": "/static/js/main.bd6447f1.chunk.js"
+  },
+  {
+    "revision": "2d8f72aeca456b536118",
+    "url": "/static/js/runtime-main.c2717fbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.574db4a3c8808d298c28b550822b33a5.js"
+  "/precache-manifest.3f9abe14b979965315dbd6a4908f03c1.js"
 );
 
 self.addEventListener('message', (event) => {
