369bcf936469fd64285a9a767b96aed22aefb1b5
nirawat <nipun.rawat@gartner.com>
2020-04-09 13:05:50 +0530

    wip

---
+++ precache-manifest.a4961d3c8ec1892190252d5e3dc3ac12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7657e13969aeec3f0b5302e10fd0ca5",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "3f93e9f2db8e1ab3dcc5",
+    "url": "/COVID-19/static/css/main.55d536c3.chunk.css"
+  },
+  {
+    "revision": "4e760ba718152805f665",
+    "url": "/COVID-19/static/js/2.19066d72.chunk.js"
+  },
+  {
+    "revision": "8a35e35e00e763aabcd15e61b0cb2343",
+    "url": "/COVID-19/static/js/2.19066d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f93e9f2db8e1ab3dcc5",
+    "url": "/COVID-19/static/js/main.b1e278df.chunk.js"
+  },
+  {
+    "revision": "80da2e9c3df50dbd7316",
+    "url": "/COVID-19/static/js/runtime-main.2bc83139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.358263929c3d5fe2823240d69b05b71d.js"
+  "/COVID-19/precache-manifest.a4961d3c8ec1892190252d5e3dc3ac12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
